2ea5c530d10bb594078256570cae34c3ec830bf0
Madung2 <tuliphan91@gmail.com>
2024-06-25 18:05:16 +0900

    ganji

---
+++ base_data/ganji.py
@@ -0,0 +1,177 @@
+GANJI = {
+    "갑": {
+        'idx': 1,
+        'korean': "갑",
+        'chinese': "甲",
+        'WuXing': "목",
+        'type': "sky", 
+        'YinYang': "양"
+    },
+    "을": {
+        'idx': 2,
+        'korean': "을",
+        'chinese': "乙",
+        'WuXing': "목",
+        'type': "sky", 
+        'YinYang': "음"
+    },
+    "병": {
+        'idx': 3,
+        'korean': "병",
+        'chinese': "丙",
+        'WuXing': "화",
+        'type': "sky", 
+        'YinYang': "양"
+    },
+    "정": {
+        'idx': 4,
+        'korean': "정",
+        'chinese': "丁",
+        'WuXing': "화",
+        'type': "sky", 
+        'YinYang': "음"
+    },
+    "무": {
+        'idx': 5,
+        'korean': "무",
+        'chinese': "戊",
+        'WuXing': "토",
+        'type': "sky", 
+        'YinYang': "양"
+    },
+    "기": {
+        'idx': 6,
+        'korean': "기",
+        'chinese': "己",
+        'WuXing': "토",
+        'type': "sky", 
+        'YinYang': "음"
+    },
+    "경": {
+        'idx': 7,
+        'korean': "경",
+        'chinese': "庚",
+        'WuXing': "금",
+        'type': "sky", 
+        'YinYang': "양"
+    },
+    "신": {
+        'idx': 8,
+        'korean': "신",
+        'chinese': "辛",
+        'WuXing': "금",
+        'type': "sky", 
+        'YinYang': "음"
+    },
+    "임": {
+        'idx': 9,
+        'korean': "임",
+        'chinese': "壬",
+        'WuXing': "수",
+        'type': "sky", 
+        'YinYang': "양"
+    },
+    "계": {
+        'idx': 10,
+        'korean': "계",
+        'chinese': "癸",
+        'WuXing': "수",
+        'type': "sky", 
+        'YinYang': "음"
+    },
+    "자": {
+        'idx': 11,
+        'korean': "자",
+        'chinese': "子",
+        'WuXing': "수",
+        'type': "land", 
+        'YinYang': "양"
+    },
+    "축": {
+        'idx': 12,
+        'korean': "축",
+        'chinese': "丑",
+        'WuXing': "토",
+        'type': "land", 
+        'YinYang': "음"
+    },
+    "인": {
+        'idx': 13,
+        'korean': "인",
+        'chinese': "寅",
+        'WuXing': "목",
+        'type': "land", 
+        'YinYang': "양"
+    },
+    "묘": {
+        'idx': 14,
+        'korean': "묘",
+        'chinese': "卯",
+        'WuXing': "목",
+        'YinYang': "음"
+    },
+    "진": {
+        'idx': 15,
+        'korean': "진",
+        'chinese': "辰",
+        'WuXing': "토",
+        'type': "land", 
+        'YinYang': "양"
+    },
+    "사": {
+        'idx': 16,
+        'korean': "사",
+        'chinese': "巳",
+        'WuXing': "화",
+        'type': "land", 
+        'YinYang': "음"
+    },
+    "오": {
+        'idx': 17,
+        'korean': "오",
+        'chinese': "午",
+        'WuXing': "화",
+        'type': "land", 
+        'YinYang': "양"
+    },
+    "미": {
+        'idx': 18,
+        'korean': "미",
+        'chinese': "未",
+        'WuXing': "토",
+        'type': "land", 
+        'YinYang': "음"
+    },
+    "신": {
+        'idx': 19,
+        'korean': "신",
+        'chinese': "申",
+        'WuXing': "금",
+        'type': "land", 
+        'YinYang': "양"
+    },
+    "유": {
+        'idx': 20,
+        'korean': "유",
+        'chinese': "酉",
+        'WuXing': "금",
+        'type': "land", 
+        'YinYang': "음"
+    },
+    "술": {
+        'idx': 21,
+        'korean': "술",
+        'chinese': "戌",
+        'WuXing': "토",
+        'type': "land", 
+        'YinYang': "양"
+    },
+    "해": {
+        'idx': 22,
+        'korean': "해",
+        'chinese': "亥",
+        'WuXing': "수",
+        'type': "land", 
+        'YinYang': "음"
+    }
+}
--- manse_api.py
+++ manse_api.py
@@ -4,6 +4,16 @@ import json
 from dotenv import load_dotenv
 import os
 
+GANJI= {
+    "갑": {
+        'idx': 1,
+        'korean': "갑",
+        'chinese': "甲",
+        'WuXing': "목",
+        'YinYang': "양"
+    }
+}
+
 class SolarToLunarAPI:
     def __init__(self):
         # .env 파일 로드
@@ -13,7 +23,11 @@ class SolarToLunarAPI:
         self.endpoint = os.getenv('ENDPOINT')
         self.api_key = os.getenv('API_KEY')
 
-    def get_lunar_solar_info(self, year: int, month: int, day: int):
+
+    def get_time_pillar_info(self, date:str, time:str):
+        pass 
+
+    def get_solar_to_lunar_info(self, year: int, month: int, day: int):
         # 요청 URL 생성
         url = f"{self.endpoint}/getLunCalInfo"
         params = {
@@ -32,15 +46,16 @@ class SolarToLunarAPI:
 
             dict_data = xmltodict.parse(xml_data)
             lunar_info = dict_data.get('response', {}).get('body', {}).get('items', {}).get('item', {})
+
             return lunar_info
         else:
             print(f"Error: {response.status_code}")
             return None
 
-# # 예제 사용법
-# api = SolarToLunarAPI()
-# year = '1991'
-# month = '07'
-# day = '25'
-# lunar_info = api.get_lunar_solar_info(year, month, day)
-# print(lunar_info)
+# 예제 사용법
+api = SolarToLunarAPI()
+year = '1991'
+month = '07'
+day = '25'
+lunar_info = api.get_solar_to_lunar_info(year, month, day)
+print(lunar_info)
